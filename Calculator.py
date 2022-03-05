434ec0f494fde571beaa6d544e0ea215a10c5ded
Gevorg Bayadyan <bayadgev@gmail.com>
2022-03-05 21:16:44 +0400

    add str future

--- Calculator.py
+++ Calculator.py
@@ -2,8 +2,7 @@
 def my_func():
     num_one = int(input("Enter your first number: "))
     num_sec = int(input("Please enter second number: "))
-
-    operation = input("Please choose preferred operator '+','-','*','/', '%'")
+    operation = input("Please choose preferred operator '+','-','*','/', '%','++': ")
     print("===============================================")
     if operation == "+":
         return num_one + num_sec
@@ -15,6 +14,8 @@ def my_func():
         return num_one / num_sec
     elif operation == "%":
             return num_one % num_sec
+    elif operation == "++":
+        return str(num_one) + str(num_sec)
     else:
         print("Run the program again")
 
