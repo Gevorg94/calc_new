dbc0953606b2cd7b15975f3ce795dbf76dfbc261
Gevorg Bayadyan <bayadgev@gmail.com>
2022-03-05 20:45:26 +0400

    add percent functionality

--- Calculator.py
+++ Calculator.py
@@ -3,7 +3,7 @@ def my_func():
     num_one = int(input("Enter your first number: "))
     num_sec = int(input("Please enter second number: "))
 
-    operation = input("Please choose preferred operator '+','-','*','/'")
+    operation = input("Please choose preferred operator '+','-','*','/', '%'")
     print("===============================================")
     if operation == "+":
         return num_one + num_sec
@@ -13,6 +13,8 @@ def my_func():
         return num_one * num_sec
     elif operation == "/":
         return num_one / num_sec
+    elif operation == "%":
+            return num_one % num_sec
     else:
         print("Run the program again")
 
