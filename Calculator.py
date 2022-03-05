1bbe9d55bb21097d67796922a66f1eea784eee49
Gevorg Bayadyan <bayadgev@gmail.com>
2022-03-05 20:41:02 +0400

    add first version of calculator

---
+++ Calculator.py
@@ -0,0 +1,21 @@
+
+def my_func():
+    num_one = int(input("Enter your first number: "))
+    num_sec = int(input("Please enter second number: "))
+
+    operation = input("Please choose preferred operator '+','-','*','/'")
+    print("===============================================")
+    if operation == "+":
+        return num_one + num_sec
+    elif operation == "-":
+        return num_one - num_sec
+    elif operation == "*":
+        return num_one * num_sec
+    elif operation == "/":
+        return num_one / num_sec
+    else:
+        print("Run the program again")
+
+
+
+print(my_func())
